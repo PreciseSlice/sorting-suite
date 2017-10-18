7a33b49364e35c757276b861f8de7e49a5a0b640
Spencer Herms <slherms@gmail.com>
2017-10-18 15:43:28 -0600

    random array generator functional

--- scripts/bubbleSort.js
+++ scripts/bubbleSort.js
@@ -1,13 +1,11 @@
 const bubbleSort = (array) => {
   for (let i = 0; i < array.length - 1; i++) {
-  	console.log(array);
     for (let j = 0; j < array.length - 1; j++) {
       if (array[j] > array[j + 1]) {
         [array[j], array[j+1]] = [array[j+1], array[j]];
       }
     }
   }
-  console.log(array);
   return array;
 };
 
--- scripts/random.js
+++ scripts/random.js
@@ -1,12 +1,20 @@
-const  createArray = () => {
-  const lowerLimValue = 1;
-  const upperLimValue = 100;
-  const arraySizeValue = 10;
+const  createArray = (lowerLimValue, upperLimValue, arraySizeValue) => {
   const resultArray = [];
   for (let i = 0 ; i < arraySizeValue; i++){
     let random = Math.floor(Math.random()*(upperLimValue-lowerLimValue) + lowerLimValue);
     resultArray.push(random);
   }
+  return resultArray;
 };
 
-module.exports = createArray;
+const checkArray = (sortedArray) => {
+	for (let i = 0; i < sortedArray.length; i ++){
+		if (sortedArray[i] <= sortedArray[i + 1]) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+};
+
+module.exports = {createArray, checkArray };
